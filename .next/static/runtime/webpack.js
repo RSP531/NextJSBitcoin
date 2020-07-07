30d7a7d56f1b603559d1a33cef1c8639850b6da7
Rob Peschke <rspesc31@gmail.com>
2020-07-07 12:20:43 -0500

    added drop down menu

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f9836a6fb3f6aeac3e57";
+/******/ 	var hotCurrentHash = "2ae1fa14600bbb2fb6a8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/Prices.js
+++ components/Prices.js
@@ -1,13 +1,13 @@
 class Prices extends React.Component {
   state = {
-    currency: "EUR"
+    currency: "USD"
   };
   render() {
     let list = "";
     if (this.state.currency === "USD") {
       list = (
         <li className="list-group-item">
-          Bitcoin Rate for{this.props.bpi.USD.description}:{" "}
+          Bitcoin Rate for {this.props.bpi.USD.description}:{" "}
           <span class="badge badge-primary">{this.props.bpi.USD.code}</span>{" "}
           <strong>{this.props.bpi.USD.rate}</strong>
         </li>
@@ -15,7 +15,7 @@ class Prices extends React.Component {
     } else if (this.state.currency === "GBP") {
       list = (
         <li className="list-group-item">
-          Bitcoin Rate for{this.props.bpi.GBP.description}:{" "}
+          Bitcoin Rate for {this.props.bpi.GBP.description}:{" "}
           <span class="badge badge-primary">{this.props.bpi.GBP.code}</span>{" "}
           <strong>{this.props.bpi.GBP.rate}</strong>
         </li>
@@ -23,7 +23,7 @@ class Prices extends React.Component {
     } else if (this.state.currency === "EUR") {
       list = (
         <li className="list-group-item">
-          Bitcoin Rate for{this.props.bpi.EUR.description}:{" "}
+          Bitcoin Rate for {this.props.bpi.EUR.description}:{" "}
           <span class="badge badge-primary">{this.props.bpi.EUR.code}</span>{" "}
           <strong>{this.props.bpi.EUR.rate}</strong>
         </li>
@@ -32,6 +32,15 @@ class Prices extends React.Component {
     return (
       <div>
         <ul className="list-group">{list}</ul>
+        <br />
+        <select
+          onChange={e => this.setState({ currency: e.target.value })}
+          className="form-control"
+        >
+          <option value="USD">USD</option>
+          <option value="GBP">GBP</option>
+          <option value="EUR">EUR</option>
+        </select>
       </div>
     );
   }
